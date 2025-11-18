d45e81e23e6c24a32c0cb23c8f9bbd9c1508618c
duyban <duybanit123@hotmail.com>
2025-11-18 22:27:31 +0700

    Update CORS

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // CHỈ 1 DOMAIN
+    'allowed_origins' => ['https://btlaptop.vercel.app'], // CHỈ 1 DOMAIN
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
