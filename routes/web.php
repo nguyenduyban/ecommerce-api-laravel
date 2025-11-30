6d552520fea7814479178219ed6e9e2816415ec6
duyban <duybanit123@hotmail.com>
2025-11-30 13:31:40 +0700

    update storage:link

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/run/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Link created!';
 });
